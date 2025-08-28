300e5b533525b39fca867acdb4977a5b3b5673bc
phmcvital <phmcvital@gmail.com>
2025-08-28 16:16:54 -0300

    terceiro commit

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+
+    return (
+        <div>
+            <h1>Solução de Treliças</h1>
+            <h3>Grupo: Pedro Vital, Eduardo, Francisco Ivan, Kaiky</h3>
+        </div>
+    )
+}
+
+export default Header
